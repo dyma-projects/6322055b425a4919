c984414894f69f7a75f1ae55c9f5d25496e0d8a8
Laurent HERVIS <laurenth@arpege.tm.fr>
2022-09-30 16:33:16 +0200

    exercice 4 : OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
 import { Exercice1Component } from './exercice1/exercice1.component';
 import { Exerice3Component } from './exerice3/exerice3.component';
+import { Exerice4Component } from './exerice4/exerice4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Exercice2Component,
     Exercice1Component,
-    Exerice3Component
+    Exerice3Component,
+    Exerice4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/exerice4/exerice4.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-exerice4",
+  templateUrl: "./exerice4.component.html",
+  styleUrls: ["./exerice4.component.css"],
+})
+export class Exerice4Component implements OnInit {
+  constructor() {}
+
+  public texts: string[] = ["un", "deux", "trois"];
+
+  ngOnInit(): void {}
+}
